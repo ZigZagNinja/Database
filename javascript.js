898ae8b28c35d48d451762baa4a943b79402113a
ZigZagNinja <36819963+ZigZagNinja@users.noreply.github.com>
2018-09-20 14:33:20 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let pressed = 0;
+document.body.addEventListener( 'keyup', function (e) {
+  if ( e.keyCode == 83 ) {
+    pressed++;
+  }
+});
+
+if(pressed === 5){
+  window.open("https://zigzagninja.github.io/Database758783878778787385888");
+}
