1a5015e705241e0432d86b39d58d341c7d7e9a70
ZigZagNinja <36819963+ZigZagNinja@users.noreply.github.com>
2018-09-20 14:42:34 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,9 +2,12 @@ let pressed = 0;
 document.body.addEventListener( 'keyup', function (e) {
   if ( e.keyCode == 83 ) {
     pressed++;
+    console.log(pressed);
   }
 });
 
 if(pressed === 5){
   window.open("https://zigzagninja.github.io/Database758783878778787385888");
 }
+
+
